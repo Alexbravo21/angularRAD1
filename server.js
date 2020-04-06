c781000f44d4dd52806bfffc165ab429974c38ad
Alejandro Bravo <alexbravo21@gmail.com>
2020-04-06 18:47:51 -0500

    AAdded the routes to the serverjs

--- server.js
+++ server.js
@@ -12,6 +12,12 @@ app.use(express.static(__dirname + '/dist/ejericio-A06032020'));
 app.get('/', function(req,res) {
     res.sendFile(path.join(__dirname+'/dist/ejericio-A06032020/index.html'));
 });
+app.get('/quoting', function(req,res) {
+    res.sendFile(path.join(__dirname+'/dist/ejericio-A06032020/index.html'));
+});
+app.get('/armory', function(req,res) {
+    res.sendFile(path.join(__dirname+'/dist/ejericio-A06032020/index.html'));
+});
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 3000);
